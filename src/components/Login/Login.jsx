0b4c43b12cffef4a5240db142333255bc4997271
shettypri <prishetty1976@gmail.com>
2023-05-17 12:48:57 +0530

    feat - loginpage

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,59 +2,61 @@
 import { useState } from "react"
 import "../../Styles/Login/Login.css"
 const Login = () => {
-    const[username, setsUername] = useState("")
+    const [username, setsUername] = useState("")
     const [password, setPassword] = useState("")
 
 
-    const handleLogin = ()=>{
-        const credentials ={
-                "username":username,
-                "password":password,
+    const handleLogin = () => {
+        const credentials = {
+            "username": username,
+            "password": password,
         }
         console.log(credentials);
     }
-  return (
-        
+    return (
 
-    <>
-    <div className="center">
-        <p>Login</p>
-        <form method="post">
-          <div className="txt_field">
-            <input type="text" required 
-            value={username}
-            onChange={
-                (e)=>{
-                    setsUername(e.target.value);
-                }
-            }
-            />
-            <span />
-            <label>Username</label>
-          </div>
-          <div className="txt_field">
-            <input type="password" required 
-            value={password}
-            onChange={
-                (e)=>{
-                    setPassword(e.target.value);
-                }
-            }
-            
-            />
-            <span />
-            <label>password</label>
-          </div>
-          <div className="pass">Forgot Password?</div>
-          <button type="submit" defaultValue="Login" 
-          onClick={handleLogin}
-          >
-            Login
-          </button>
-        </form>
-      </div>
-    </>
-  )
+
+        <>
+            <div className="login-main">
+                <div className="center">
+                    <p>Login</p>
+                    <form method="post">
+                        <div className="txt_field">
+                            <input type="text" required
+                                value={username}
+                                onChange={
+                                    (e) => {
+                                        setsUername(e.target.value);
+                                    }
+                                }
+                            />
+                            <span />
+                            <label>Username</label>
+                        </div>
+                        <div className="txt_field">
+                            <input type="password" required
+                                value={password}
+                                onChange={
+                                    (e) => {
+                                        setPassword(e.target.value);
+                                    }
+                                }
+
+                            />
+                            <span />
+                            <label>password</label>
+                        </div>
+                        <div className="pass">Forgot Password?</div>
+                        <button type="submit" defaultValue="Login"
+                            onClick={handleLogin}
+                        >
+                            Login
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Login
