a723272393e563059c0479a8351111fe2b99600e
shettypri <prishetty1976@gmail.com>
2023-05-17 21:13:31 +0530

    changes

---
+++ src/components/admin/Account/Account.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Account = () => {
+  return (
+    <>
+    <div className="main-dashboard">
+        <div className="card1">
+            <h1>hellp</h1>
+
+        </div>
+        <div className="card2">
+
+        </div>
+        <div className="card3">
+
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Account
