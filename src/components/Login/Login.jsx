c0f12c938885468d8faa279e3e0625e4d9509053
AJ <joysondsouzaaj@gmail.com>
2023-05-19 20:19:42 +0530

    Login jsx code modified

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,64 +2,57 @@
 import { useState } from "react"
 import "../../Styles/Login/Login.css"
 const Login = () => {
-    const [username, setsUername] = useState("")
-    const [password, setPassword] = useState("")
+  const [loginCredentials, setloginCredentials] = useState({
+    "username": "",
+    "password": ""
+  })
 
-    const [loginCredentials, setloginCredentials] = useState({
-      "username":"",
-      "password":""
+  const handleChange = (e) =>{
+    setloginCredentials({
+      ...loginCredentials,[e.target.name]:e.target.value
     })
+  }
 
-    const handleLogin = () => {
-        const credentials = {
-            "username": username,
-            "password": password,
-        }
-        console.log(credentials);
-    }
+  const handleLogin = () => {
+    console.log(loginCredentials);
+  }
   return (
     <>
-    <div className="login-main">
-    <div className="center">
-        <p>Login</p>
-        <form method="post">
-          <div className="txt_field">
-            <input type="text" required 
-            value={username}
-            onChange={
-                (e)=>{
-                    setsUername(e.target.value);
-                }
-            }
-            />
-            <span />
-            <label>Username</label>
+      <div className="login-main">
+        <div className="center">
+          <p>Login</p>
+          <div>
+            <div className="txt_field">
+              <input type="text" required
+                value={loginCredentials.username}
+                name="username"
+                onChange={handleChange}
+              />
+              <span />
+              <label>Username</label>
+            </div>
+            <div className="txt_field">
+              <input type="password" required
+                value={loginCredentials.password}
+                name="password"
+                onChange={handleChange}
+              />
+              <span />
+              <label>password</label>
+            </div>
+            {/* <div className="pass">Forgot Password?</div> */}
+            <div className="login-btn">
+              <button type="submit" defaultValue="Login"
+                onClick={handleLogin}
+              >
+                Login
+              </button>
+            </div>
+
           </div>
-          <div className="txt_field">
-            <input type="password" required 
-            value={password}
-            onChange={
-                (e)=>{
-                    setPassword(e.target.value);
-                }
-            }
-            />
-            <span />
-            <label>password</label>
-          </div>
-          {/* <div className="pass">Forgot Password?</div> */}
-          <div className="login-btn">
-          <button type="submit" defaultValue="Login" 
-          onClick={handleLogin}
-          >
-            Login
-          </button>
-          </div>
-          
-        </form>
+        </div>
       </div>
-    </div>
-    
+
     </>
   )
 }
