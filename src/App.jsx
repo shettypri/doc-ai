91a6585f75b88d30a253af7d9cbbb6776c24757d
AJ <joysondsouzaaj@gmail.com>
2023-05-25 12:17:14 +0530

    Registeration Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Form, Route, Routes, } from 'react-router-dom'
+import {BrowserRouter, Form, Route, Routes,} from 'react-router-dom'
 import './App.css'
 import Navbar from './components/doctor/01_Navbar/Navbar'
 import Home from './components/doctor/02_Home/Home'
@@ -15,46 +15,45 @@ import PendingReq from './components/admin/Account/PendingReq'
 
 
 function App() {
-  const navbarList = [
-    "Research", "Publication", "Projects", "About-Us", "Contact-Us"
-  ]
-  return (
-    <>
+    const navbarList = [
+        "Research", "Publication", "Projects", "About-Us", "Contact-Us"
+    ]
+    return (
+        <>
 
-    <BrowserRouter>
-      <div>
-      <Navbar/>
-      </div>
-      <Routes>
+            <BrowserRouter>
+                <div>
+                    <Navbar/>
+                </div>
+                <Routes>
 
-        {/* Testing path */}
-        <Route exact path='/PendingReq' element={<PendingReq />} />
-        {/* Testing path ends */}
-        {/* <Route exact path='/form' element={<Form />}/> */}
-        <Route exact path="/" element={<Home />}/>
-        <Route exact path="/Research" element={<Research />} />
-        <Route exact path='/Publication' element={<Publication />}/>
-        <Route exact path='/Contact-Us' element={<Contact />} />
-        <Route exact path='/login' element={<Login />} />
-        <Route exact path='/register' element={<Register />} />
-        <Route exact path='/Dashboard/*' element={<Dashboard />} />
-        <Route exact path='/Account'  element={<Account/>}/>
-        <Route exact path='/Dashboard/Form'  element={<FormPage />}/>
-        <Route exact path='/Dashboard/researchForm' element={<ResearchForm />} />
+                    {/* Testing path */}
+                    <Route exact path='/PendingReq' element={<PendingReq/>}/>
+                    {/* Testing path ends */}
+                    {/* <Route exact path='/form' element={<Form />}/> */}
+                    <Route exact path="/" element={<Home/>}/>
+                    <Route exact path="/Research" element={<Research/>}/>
+                    <Route exact path='/Publication' element={<Publication/>}/>
+                    <Route exact path='/Contact-Us' element={<Contact/>}/>
+                    <Route exact path='/login' element={<Login/>}/>
+                    <Route exact path='/register' element={<Register/>}/>
+                    <Route exact path='/Dashboard/*' element={<Dashboard/>}/>
+                    <Route exact path='/Account' element={<Account/>}/>
+                    <Route exact path='/Dashboard/Form' element={<FormPage/>}/>
+                    <Route exact path='/Dashboard/researchForm' element={<ResearchForm/>}/>
 
-        
-        {/* <Route exact path='/publisher'  element={<publisher/>}/>
+
+                    {/* <Route exact path='/publisher'  element={<publisher/>}/>
         <Route exact path='/researcher'  element={<researcher/>}/> */}
 
 
-    
-        {/* <Route exact path='/' */}
-      </Routes>
-    </BrowserRouter>
-      
-      
-    </>
-  )
+                    {/* <Route exact path='/' */}
+                </Routes>
+            </BrowserRouter>
+
+
+        </>
+    )
 }
 
 export default App
