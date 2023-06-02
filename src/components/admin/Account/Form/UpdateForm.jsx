408694e78c1c4ef3209f73ec708c59a02b489b8d
Poojagsb <ppoojabhakta@gmail.com>
2023-06-02 11:04:16 +0530

    updateform updated

--- src/components/admin/Account/Form/UpdateForm.jsx
+++ src/components/admin/Account/Form/UpdateForm.jsx
@@ -1,5 +1,5 @@
 // import React from 'react'
-import  { useState } from 'react'
+import { useState } from 'react'
 import "../../../../Styles/admin/Account/Form/UpdateForm.css"
 import { uploadImgToStore } from '../../../../components/Register/docCRUD.js'
 import { auth } from '../../../../config/firebase-config';
@@ -19,12 +19,12 @@ const UpdateForm = () => {
         "Specialization": "",
         "doctorId": "",
         "imageUrl": "",
-        isFormFilled:true,
+        isFormFilled: true,
         isAdmin: false,
-        isDocAuthorized:false
+        isDocAuthorized: false
     })
     const [docImage, setDocImage] = useState("")
-    
+
     const doctorValue = (e) => {
         setDoctorDetails({
             ...doctorDetails, [e.target.name]: e.target.value
@@ -36,7 +36,7 @@ const UpdateForm = () => {
         try {
             // const imageUrl = await uploadImgToStore(docImage, docImage.name)
             setDoctorDetails({
-                ...doctorDetails,["imageUrl"]:await uploadImgToStore(docImage, docImage.name)
+                ...doctorDetails, ["imageUrl"]: await uploadImgToStore(docImage, docImage.name)
             })
             console.log("After updating the values");
             console.log(doctorDetails);
@@ -45,21 +45,21 @@ const UpdateForm = () => {
         }
     }
 
-  return (
-    <>
-    <div className="containerus">
-    <div className="titleus">update details</div><br />
-    <form action="#">
-        <div className="user-detailsus">
-            <div className="input-boxus">
-                <span className="detailsus">Full Name</span>
-                <input type="text"
-                    name='firstName' disabled
-                    onChange={doctorValue}
-                    placeholder="Enter First Name" required />
-            </div>
+    return (
+        <>
+            <div className="containerus">
+                <div className="titleus">update details</div><br />
+                <form action="#">
+                    <div className="user-detailsus">
+                        <div className="input-boxus">
+                            <span className="detailsus">Full Name</span>
+                            <input type="text"
+                                name='FullName' disabled
+                                onChange={doctorValue}
+                                placeholder="Enter Full Name" required />
+                        </div>
 
-            {/* <div className="input-boxus">
+                        {/* <div className="input-boxus">
                 <span className="detailsus">Last Name</span>
                 <input type="text" disabled
                     placeholder="Enter Last Name"
@@ -68,68 +68,76 @@ const UpdateForm = () => {
                     required />
             </div> */}
 
-            <div className="input-boxus">
+                        {/* <div className="input-boxus">
                 <span className="detailsus">DOB</span>
                 <input type="date"
                     name='dateOfBirth' disabled
                     onChange={doctorValue}
                     placeholder="Enter Date of Birth" required />
-            </div>
+            </div> */}
 
-            <div className="input-boxus">
-                <span className="detailsus">Email</span>
-                <input type="text"
-                    name='email' 
-                    onChange={doctorValue}
-                    placeholder="Enter Email"
-                    required />
-            </div>
+                        <div className="input-boxus">
+                            <span className="detailsus">Email</span>
+                            <input type="text"
+                                name='email'
+                                onChange={doctorValue}
+                                placeholder="Enter Email"
+                                required />
+                        </div>
 
-            <div className="input-boxus">
-                <span className="detailsus">Phone No</span>
-                <input type="text"
-                    name='phoneNumber'
-                    onChange={doctorValue}
-                    placeholder="Enter Phone number"
-                    maxLength={10}
-                    required />
-            </div>
-            <div className="input-boxus">
+                        <div className="input-boxus">
+                            <span className="detailsus">Phone No</span>
+                            <input type="text"
+                                name='phoneNumber'
+                                onChange={doctorValue}
+                                placeholder="Enter Phone number"
+                                maxLength={10}
+                                required />
+                        </div>
+                        <div className="input-boxus">
+                            <span className="detailsus">Hospital</span>
+                            <input type="text"
+                                placeholder="Enter Hospital"
+                                name='hospital'
+                                onChange={doctorValue}
+                                required />
+                        </div>
+                        {/* <div className="input-boxus">
                 <span className="detailsus">Address</span>
                 <textarea type="text" placeholder="Enter Address" required style={{ width: '300px', height: '70px' }} defaultValue={""} />
-            </div>
-            <div className="input-boxus">
-                <span className="detailsus">City</span>
-                <input type="text"
-                    name='city'
-                    onChange={doctorValue}
-                    placeholder="Enter City" required />
-            </div>
+            </div> */}
+                        <div className="input-boxus">
+                            <span className="detailsus">City</span>
+                            <input type="text"
+                                name='city'
+                                onChange={doctorValue}
+                                placeholder="Enter City" required />
+                        </div>
 
-            <div className="input-boxus">
+                        {/* <div className="input-boxus">
                 <span className="detailsus">Pincode</span>
                 <input type="text"
                     name='pincode'
                     onChange={doctorValue}
                     placeholder="Enter Pincode" required />
-            </div>
+            </div> */}
 
-            <div className="input-boxus">
-                <span className="detailsus">Specialization</span>
-                <input type="text" disabled
-                    name='Specialization'
-                    onChange={handleDetails}
-                    placeholder="Enter  Specialization" required />
-            </div>
+                        <div className="input-boxus">
+                            <span className="detailsus">Specialization</span>
+                            <input type="text" disabled
+                                name='Specialization'
+                                onChange={handleDetails}
+                                placeholder="Enter  Specialization" required />
+                        </div>
 
-            <div className="input-boxus">
+                        {/* <div className="input-boxus">
                 <span className="detailsus">Doctor ID</span>
                 <input type="text" disabled
                     onChange={handleDetails}
                     name='doctorId'
                     placeholder="Enter Doctor ID" required />
-            </div>
-            <div className="imagepreviewus">
+            </div> */}
+                        {/* <div className="imagepreviewus">
                 <input
                     type="file"
                     accept="image/*"
@@ -143,19 +151,19 @@ const UpdateForm = () => {
                 <span id="file-chosen">
                     No file chosen
                 </span>
+            </div> */}
+                        <div className="buttonus">
+                            <input type="submit"
+                                value={"submit"}
+                                onClick={handleDetails} />
+                        </div>
+                    </div>
+                </form>
             </div>
-            <div className="buttonus">
-                <input type="submit"
-                    value={"submit"}
-                    onClick={handleDetails} />
-            </div>
-        </div>
-    </form>
-</div>
-</>
-       
-   
-  )
+        </>
+
+
+    )
 }
 
 export default UpdateForm
